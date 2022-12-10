1fa518df404af3614d8bf9c67fbd34350123c07b
MiniShark27 <kobifalus@gmail.com>
2022-12-09 23:15:43 -0500

    end of night changes

--- DCCNN.py
+++ DCCNN.py
@@ -6,70 +6,56 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
 
-        # Convolutional layers
-        self.fork1 = nn.Sequential(
-            nn.Conv2d(in_channels=1,
-                      out_channels=64,
-                      kernel_size=5,
-                      stride=1,
-                      padding='same'
-                      ),
-            nn.ReLU(),
-            nn.BatchNorm2d(64)
-        )
+        conv11 = nn.Conv2d(1, 64, 5, padding='same')
+        conv12 = nn.Conv2d(1, 64, 5, padding='same')
+        conv21 = nn.Conv2d(64, 128, 4, padding='same')
+        conv22 = nn.Conv2d(64, 128, 4, padding='same')
+        conv31 = nn.Conv2d(128, 256, 3, padding='same')
+        conv32 = nn.Conv2d(128, 256, 3, padding='same')
+        conv33 = nn.Conv2d(128, 256, 3, padding='same')
+        conv34 = nn.Conv2d(128, 256, 3, padding='same')
+        conv35 = nn.Conv2d(128, 256, 3, padding='same')
+        conv36 = nn.Conv2d(128, 256, 3, padding='same')
 
-        self.fork2 = nn.Sequential(
-            nn.Conv2d(in_channels=64,
-                      out_channels=128,
-                      kernel_size=4,
-                      stride=1,
-                      padding='same'
-                      ),
-            nn.ReLU(),
-            nn.BatchNorm2d(128)
-        )
+        self.fork_1_1 = nn.Sequential(conv11, nn.ReLU(), nn.BatchNorm2d(64))
+        self.fork_1_2 = nn.Sequential(conv12, nn.ReLU(), nn.BatchNorm2d(64))
+        self.fork_2_1 = nn.Sequential(conv21, nn.ReLU(), nn.BatchNorm2d(128))
+        self.fork_2_2 = nn.Sequential(conv22, nn.ReLU(), nn.BatchNorm2d(128))
+        self.fork_3_1 = nn.Sequential(conv31, nn.ReLU(), nn.BatchNorm2d(256))
+        self.fork_3_2 = nn.Sequential(conv32, nn.ReLU(), nn.BatchNorm2d(256))
+        self.fork_3_3 = nn.Sequential(conv33, nn.ReLU(), nn.BatchNorm2d(256))
+        self.fork_3_4 = nn.Sequential(conv34, nn.ReLU(), nn.BatchNorm2d(256))
+        self.fork_3_5 = nn.Sequential(conv35, nn.ReLU(), nn.BatchNorm2d(256))
+        self.fork_3_6 = nn.Sequential(conv36, nn.ReLU(), nn.BatchNorm2d(256))
 
-        self.fork3 = nn.Sequential(
-            nn.Conv2d(in_channels=128,
-                      out_channels=256,
-                      kernel_size=3,
-                      stride=1,
-                      padding='same'
-                      ),
-            nn.ReLU(),
-            nn.BatchNorm2d(256)
-        )
-
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2, padding='same')
+        self.max_pool_1 = nn.MaxPool2d(kernel_size=2, stride=2, padding='same')
+        self.max_pool_2 = nn.MaxPool2d(kernel_size=2, stride=2, padding='same')
+        self.max_pool_3 = nn.MaxPool2d(kernel_size=2, stride=2, padding='same')
 
         self.dropout = nn.Dropout(p=0.5)
 
-        # Fully connected layer
-        self.out = nn.Sequential(
-                nn.Linear(1080, 10),  # fix dims later
-                nn.Softmax()
-        )
-
+        self.dense = nn.Sequential(nn.Linear(1080, 10), nn.Softmax())
 
     def forward(self, batch):
-        x1=self.fork1(batch)
-        x2=self.fork1(batch)
-        layer1=torch.cat((x1, x2), dim=1)
-        layer1=self.maxpool(layer1)
-        x1=self.fork2(layer1)
-        x2=self.fork2(layer1)
-        layer2=torch.cat((x1, x2), dim=1)
-        layer2=self.maxpool(layer2)
-        x1=self.fork3(layer2)
-        x2=self.fork3(layer2)
-        x3=self.fork3(layer2)
-        x4=self.fork3(layer2)
-        x5=self.fork3(layer2)
-        x6=self.fork3(layer2)
-        layer3=torch.cat((x1, x2, x3, x4, x5, x6), dim=1)
-        layer3=self.maxpool(layer3)
+        x1 = self.fork_1_1(batch)
+        x2 = self.fork_1_2(batch)
+        l1 = torch.cat((x1, x2), dim=1)
+        l1 = self.max_pool_1(l1)
 
-        layer3=layer3.view(layer3.size(0), -1)  # maybe not needed
-        dropout=self.dropout(layer3)
-        output=self.out(dropout)
+        x1 = self.fork_2_1(l1)
+        x2 = self.fork_2_2(l1)
+        l2 = torch.cat((x1, x2), dim=1)
+        l2 = self.max_pool_2(l2)
+
+        x1 = self.fork_3_1(l2)
+        x2 = self.fork_3_2(l2)
+        x3 = self.fork_3_3(l2)
+        x4 = self.fork_3_4(l2)
+        x5 = self.fork_3_5(l2)
+        x6 = self.fork_3_6(l2)
+        l3 = torch.cat((x1, x2, x3, x4, x5, x6), dim=1)
+        l3 = self.max_pool_3(l3)
+
+        l3 = l3.view(l3.size(0), -1)  # maybe not needed
+        output = self.dense(l3)
         return output
--- utils.py
+++ utils.py
@@ -2,28 +2,31 @@
 
 
 # Get batch of images:
+import matplotlib.pyplot as plt
+import gzip
 import cv2
 import os
 import numpy as np
 # from sklearn.datasets import fetch_openml
 
 _handwritten_key = dict()
-_handwritten_path = os.path.join('handwritten_math')
-_aida_path = os.path.join('aida')
-_mnist_path = os.path.join('mnist')
+_handwritten_path = os.path.join('datasets', 'handwritten')
+_aida_path = os.path.join('datasets', 'aida')
+_mnist_path = os.path.join('datasets', 'mnist')
 _handwritten_paths = []
+
 
 def get_handwritten_keys(labels=None):
     if not _handwritten_key:
         img_types = os.listdir(_handwritten_path)
-        for i,img_type in enumerate(img_types):
+        for i, img_type in enumerate(img_types):
             _handwritten_key[img_type] = i
     nums = []
     for label in labels:
         nums.append(_handwritten_key[label])
     return np.array(nums)
 
-    
+
 def open_image(path):
     """Open image from path
 
@@ -32,9 +35,10 @@ def open_image(path):
 
     Returns:
         Mat: image
-    """    
+    """
     img = cv2.imread(path)
     return img
+
 
 def standardize_image(img, invert=False, resize=False, to_gray=False):
     """Standardize image by converting to grayscale and resizing
@@ -45,10 +49,10 @@ def standardize_image(img, invert=False, resize=False, to_gray=False):
 
     Returns:
         Mat: standardized image
-    """    
-    SIZE = (28,28)
+    """
+    SIZE = (28, 28)
     if resize:
-        img = cv2.resize(img,SIZE)
+        img = cv2.resize(img, SIZE)
     if to_gray:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).astype(np.uint8)
     if invert:
@@ -64,64 +68,68 @@ def get_aida_batch(batch_id):
 
     Yields:
         Mat: image
-    """        
-    path = os.path.join(_aida_path,f'batch_{batch_id}','background_images')
+    """
+    path = os.path.join(_aida_path, f'batch_{batch_id}', 'background_images')
     img_names = os.listdir(path)
     for img_name in img_names:
-        img_path = os.path.join(path,img_name)
+        img_path = os.path.join(path, img_name)
         img = open_image(img_path)
         img = standardize_image(img, to_gray=True)
         yield img
-        
-def get_handwritten_batch(batch_id,batch_count):
+
+
+def get_handwritten_batch(batch_count, batch_min, batch_max=None):
     """Gets a batch of handwritten images and their values, returns a generator.
 
     Args:
-        batch_id (int): batch id (must be less than batch_count)
         batch_count (int): number of batches total
+        batch_min (int): batch id (must be less than batch_count)
 
     Yields:
         tuple[str,Mat]: The Label and the image
-    """    
+    """
+    if batch_max is None:
+        batch_max = batch_min + 1
     # Initialize paths
     if not _handwritten_paths:
         img_types = os.listdir(_handwritten_path)
         for img_type in img_types:
-            img_type_path = os.path.join(_handwritten_path,img_type)
+            img_type_path = os.path.join(_handwritten_path, img_type)
             img_names = os.listdir(img_type_path)
             for img_name in img_names:
-                img_path = os.path.join(img_type_path,img_name)
-                _handwritten_paths.append((img_type,img_path))
+                img_path = os.path.join(img_type_path, img_name)
+                _handwritten_paths.append((img_type, img_path))
         np.random.shuffle(_handwritten_paths)
-        
+
     batch_size = len(_handwritten_paths)//batch_count
-    
-    # Get batch and return 
-    batch = _handwritten_paths[batch_id*batch_size:(batch_id+1)*batch_size]
+
+    # Get batch and return
+    batch = _handwritten_paths[batch_min * batch_size:batch_max*batch_size]
     print(len(batch))
     labels = []
     images = []
-    for img_type,img_path in batch:
+    for img_type, img_path in batch:
         img = open_image(img_path)
         img = standardize_image(img, invert=True, to_gray=True, resize=True)
         labels.append(img_type)
         images.append(img)
-    return np.array(labels),np.array(images)
+    return np.array(labels), np.array(images)
+
 
 def show_img(img):
     """Displays an image
 
     Args:
         img (Mat): image to display
-    """    
+    """
     cv2.imshow('image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
-import gzip
+
+
 def read_mnist():
-    path = os.path.join(_mnist_path,'t10k-images-idx3-ubyte.gz')
-    with gzip.open(path,'r') as f:
+    path = os.path.join(_mnist_path, 't10k-images-idx3-ubyte.gz')
+    with gzip.open(path, 'r') as f:
         image_size = 28
         num_images = 10000
         f.read(16)
@@ -129,21 +137,55 @@ def read_mnist():
         data = np.frombuffer(buffer, dtype=np.uint8).astype(np.float32)
         print(data.shape)
         data = data.reshape(num_images, image_size, image_size)
-    with gzip.open(os.path.join(_mnist_path,'t10k-labels-idx1-ubyte.gz'),'r') as f:
+    with gzip.open(os.path.join(_mnist_path, 't10k-labels-idx1-ubyte.gz'), 'r') as f:
         f.read(8)
         buffer = f.read(10000)
         labels = np.frombuffer(buffer, dtype=np.uint8).astype(np.int64)
-    return labels,data
+    return labels, data
 
-import matplotlib.pyplot as plt
-    
+# Evaluate the accuracy of labels
+
+
+def evaluateLabels(y, ypred, visualize=True):
+
+    classLabels = np.unique(y)
+    conf = np.zeros((len(classLabels), len(classLabels)))
+    for tc in range(len(classLabels)):
+        for pc in range(len(classLabels)):
+            conf[tc, pc] = np.sum(np.logical_and(y == classLabels[tc],
+                                                 ypred == classLabels[pc]).astype(float))
+
+    acc = np.sum(np.diag(conf))/y.shape[0]
+
+    if visualize:
+        plt.figure()
+        plt.imshow(conf, cmap='gray')
+        plt.ylabel('true labels')
+        plt.xlabel('predicted labels')
+        plt.title('Confusion matrix (Accuracy={:.2f})'.format(acc*100))
+        plt.show()
+
+    return (acc, conf)
+
+
+def one_hot(y, num_classes):
+    """One hot encoding of y
+
+    Args:
+        y (np.array): labels
+        num_classes (int): number of classes
+
+    Returns:
+        np.array: one hot encoded labels
+    """
+    return np.eye(num_classes)[y]
+
+
 if __name__ == '__main__':
-    l,d = read_mnist()
-    print(l.shape,d.shape)
+    l, d = read_mnist()
+    print(l.shape, d.shape)
     for _ in range(10):
         print(l[_])
         img = d[_]
         plt.imshow(img)
         plt.show()
-    
-    
